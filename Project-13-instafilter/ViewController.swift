14cb156f3b91ceb7bc6e2b336408b3caf97c8ceb
kevin cuadros <kevindavidcuadros@gmail.com>
2024-09-11 17:39:44 -0500

    Create UI and add event

--- Project-13-instafilter/ViewController.swift
+++ Project-13-instafilter/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensity: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func intensityChange(_ sender: Any) {
+    }
+    
 
-
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    
 }
 
