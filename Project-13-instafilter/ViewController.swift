aa33f7b3a172e89cf1e2218cb9bfe95123e5b14c
kevin cuadros <kevindavidcuadros@gmail.com>
2024-09-12 16:36:34 -0500

    Save Image photos album

--- Project-13-instafilter/ViewController.swift
+++ Project-13-instafilter/ViewController.swift
@@ -55,6 +55,13 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
     
     
     @IBAction func save(_ sender: Any) {
+        guard let image = imageView.image else { return }
+        UIImageWriteToSavedPhotosAlbum(
+            image,
+            self,
+            #selector(image(_:didFinishSavingWithError:contextInfo:)),
+            nil
+        )
     }
     
     @objc func selectImage(){
@@ -110,6 +117,19 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
         currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
         applyProcessing()
         
+    }
+    
+    @objc func image( _ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer ){
+        if let error = error {
+            // we got back an error!
+            let ac = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        } else {
+            let ac = UIAlertController(title: "Saved!", message: "Your altered image has been saved to your photos.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
     }
     
 }
