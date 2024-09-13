532031ddef8da5341dc9c0a592f546046a8cfda0
kevin cuadros <kevindavidcuadros@gmail.com>
2024-09-13 17:41:43 -0500

    Add Slider ratio add one filter

--- Project-13-instafilter/ViewController.swift
+++ Project-13-instafilter/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 import CoreImage
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+    @IBOutlet var ratio: UISlider!
+    @IBOutlet var filterButton: UIButton!
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var intensity: UISlider!
     
@@ -33,7 +35,11 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
         applyProcessing()
     }
     
-
+    @IBAction func ratioChange(_ sender: Any) {
+        applyProcessing()
+    }
+    
+    
     @IBAction func changeFilter(_ sender: UIButton) {
         let ac = UIAlertController(title: "Choose filter", message: nil, preferredStyle: .actionSheet)
         ac.addAction(UIAlertAction(title: "CIBumpDistortion", style: .default, handler: setFilter))
@@ -43,6 +49,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
         ac.addAction(UIAlertAction(title: "CITwirlDistortion", style: .default, handler: setFilter))
         ac.addAction(UIAlertAction(title: "CIUnsharpMask", style: .default, handler: setFilter))
         ac.addAction(UIAlertAction(title: "CIVignette", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIZoomBlur", style: .default, handler: setFilter))
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         
         if let popoverController = ac.popoverPresentationController {
@@ -55,7 +62,16 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
     
     
     @IBAction func save(_ sender: Any) {
-        guard let image = imageView.image else { return }
+        guard let image = imageView.image else {
+            let ac = UIAlertController(
+                title: "Don't Save",
+                message: "Select an image to save",
+                preferredStyle: .alert
+            )
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+            return
+        }
         UIImageWriteToSavedPhotosAlbum(
             image,
             self,
@@ -90,14 +106,19 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
             currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey)
         }
         if inputKeys.contains(kCIInputRadiusKey) {
-            currentFilter.setValue(intensity.value * 200, forKey: kCIInputRadiusKey)
+            currentFilter.setValue(ratio.value * 200, forKey: kCIInputRadiusKey)
         }
         if inputKeys.contains(kCIInputScaleKey) {
             currentFilter.setValue(intensity.value * 10, forKey: kCIInputScaleKey)
         }
+        if inputKeys.contains(kCIInputAmountKey){
+            currentFilter.setValue(intensity.value * 50, forKey: kCIInputAmountKey)
+        }
         if inputKeys.contains(kCIInputCenterKey) {
             currentFilter.setValue(CIVector(x: currentImage.size.width / 2, y: currentImage.size.height / 2), forKey: kCIInputCenterKey)
         }
+        
+        
         
         if let cgimg = context.createCGImage(image, from: image.extent) {
             let processedImage = UIImage(cgImage: cgimg)
@@ -110,6 +131,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
         guard currentImage != nil else { return }
         
         guard let actionTitle = action.title else { return }
+        
+        filterButton.setTitle(actionTitle, for: .normal)
         
         currentFilter = CIFilter(name: actionTitle)
         
